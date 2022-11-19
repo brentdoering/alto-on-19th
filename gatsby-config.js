950de70b9b3dd6801a01a981b9e05389540550a3
Brent Doering <brentiscoding@gmail.com>
2022-11-18 21:21:18 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Alto on 19th`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
